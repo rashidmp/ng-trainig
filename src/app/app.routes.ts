d29c2a78bea0b1275ddbb09d2b3fb8d01b248d1d
Muhammed Rashid M P <alrashid4star@gmail.com>
2025-10-18 16:08:52 +0530

    day 1 - 18/10/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: About, title: 'About Us' },
+  { path: 'contact', component: Contact, title: 'Contact Us' },
+];
---
+++ src/app/components/contact/contact.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  template: `
+    <div class="h-screen flex items-center justify-center">
+      <p class="text-6xl text-purple-500">Contact Us</p>
+    </div>
+  `,
+  styles: ``,
+})
+export class Contact {}
